f4e05874c8d554e6535a47f8473ac121b5dbeca2
MohsenKazemizade <e.m.kazemizade@gmail.com>
2018-06-28 10:43:00 +0430

    Initial commit

---
+++ app/src/main/java/com/example/asus/firstapp11/MainActivityMK.java
@@ -0,0 +1,14 @@
+package com.example.asus.firstapp11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityMK extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.apple_calulator);
+    }
+
+}
